05f89d04d97c5e79978c605fb9fba4b6fa742d2d
harshan balasubramanian <aishan@192.168.1.7>
2025-01-30 04:35:59 +0530

    chore: proxy vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/api/, '/api')
+      },
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
